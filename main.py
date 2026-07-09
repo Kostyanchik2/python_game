e957e86ea3088d33c9705326826a200a22684dbe
Kostyanchik2 <kostya_kravtsov_2024@vk.com>
2026-07-09 19:06:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ def ru():
       print("cd (Не писать дальше!) - перейти в деректорию")
       print("dir - Посмотреть файлы в деректории") 
       print("Pokemon - ЧТО ЖЕ ЭТО ЗА ПОКЕМОН!? (Автор не присваевает себе права на франшизу Pockemon")
+      
 def VHOD():
+  global Root
   global Root_вход
   Root_вход = input("Для прав администратора введите пароль(для ограниченных прав напишите N")
   if Root_вход == password:
-    root = "True"
+    root = True
     ru()
-  elif Root_вход == "N"
+  elif Root_вход == "N":
     ru()
-  elif Root_вход != password
+  elif Root_вход != password:
     print("Не верный пароль, повторите попытку")
     VHOD()
     
@@ -30,7 +32,7 @@ def VHOD():
 directory = "C:/"
 name = input("Хотите создать учетную запись? (Want to create an account?) Y/N").lower()
 password = ""
-root = "False"
+root = False
 if name == "Y":
   name = input("Имя учётной записи (Account name): ") 
   password = input("введите пароль")
