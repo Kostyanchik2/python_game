d2eb918b0758e8cdc9eccb727c3d179cf66472e8
Kostyanchik2 <kostya_kravtsov_2024@vk.com>
2026-07-09 19:02:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,26 @@
 def ru():
   global cmd
   print(f"{name} , Добро пожаловать!")
+  Vhod = input("Введите ваш пароль")
   while True:
-  cmd = input(f"{directory} > ").lower()
-  if cmd == "help":
-    print("cd (Не писать дальше!) - перейти в деректорию")
-    print("dir - Посмотреть файлы в деректории") 
-    print("Pockemon - ЧТО ЖЕ ЭТО ЗА ПОКЕМОН!? (Автор не присваевает себе права на франшизу Pockemon")
+    cmd = input(f"{directory} > ").lower()
+    if cmd == "help":
+      print("cd (Не писать дальше!) - перейти в деректорию")
+      print("dir - Посмотреть файлы в деректории") 
+      print("Pokemon - ЧТО ЖЕ ЭТО ЗА ПОКЕМОН!? (Автор не присваевает себе права на франшизу Pockemon")
+def VHOD():
+  global Root_вход
+  Root_вход = input("Для прав администратора введите пароль(для ограниченных прав напишите N")
+  if Root_вход == password:
+    root = "True"
+    ru()
+  elif Root_вход == "N"
+    ru()
+  elif Root_вход != password
+    print("Не верный пароль, повторите попытку")
+    VHOD()
+    
+  
     
     
   
@@ -15,12 +29,16 @@ def ru():
   
 directory = "C:/"
 name = input("Хотите создать учетную запись? (Want to create an account?) Y/N").lower()
-if name == Y:
+password = ""
+root = "False"
+if name == "Y":
   name = input("Имя учётной записи (Account name): ") 
-  ru()
+  password = input("введите пароль")
+  VHOD()
 else:
-  name = User228
+  name = "User228"
   ru()
+  
   
  
   
