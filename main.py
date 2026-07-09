2f1fe067ecf9dfe05d4b34aacb901ef6260055a6
Kostyanchik2 <kostya_kravtsov_2024@vk.com>
2026-07-09 18:21:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 # ТЫ ЗАЧЕМ СЮДА ЗАШЕЛ ЕМОЕ
+def ru():
+  global cmd
+  print(f"{name} , Добро пожаловать!")
+  while True:
+  cmd = input(f"{directory} > ").lower()
+  if cmd == "help":
+    print("cd (Не писать дальше!) - перейти в деректорию")
+    print("dir - Посмотреть файлы в деректории") 
+    print("Pockemon - ЧТО ЖЕ ЭТО ЗА ПОКЕМОН!? (Автор не присваевает себе права на франшизу Pockemon")
+    
+    
+  
+  
+  
 directory = "C:/"
 name = input("Хотите создать учетную запись? (Want to create an account?) Y/N").lower()
-if name == 
+if name == Y:
+  name = input("Имя учётной записи (Account name): ") 
+else:
+  ru()
+  
+ 
+  
+  
+    
