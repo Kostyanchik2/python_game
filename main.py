52b0a193add1427f63550fa70f51b3c3a5195fa3
Kostyanchik2 <kostya_kravtsov_2024@vk.com>
2026-07-12 14:07:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# доделать блокнот надо
 import winsound 
 import time
 Directory = "C:/"
@@ -8,10 +9,21 @@ Folder_two_C = "system32"
 File_two_C = "None"
 Folder_three_C = "None"
 papka = 0
+notepad = ""
 def Ru():
+    global File_one_users
+    global Folder_one_users
+    Folder_one_users = name
+    global notepad 
+    global File_one_C
+    global Folder_one_C
+    global Folder_two_C
+    global File_two_C
+    global Folder_three_C
+    global papka
     while True:
         cmd = input(f"{Directory} > ")
-        if cmd == "":
+        if cmd == "Null":
             print("Неизвестная ошибка")
             winsound.Beep(1000, 1000)
             time.sleep(5)
@@ -30,6 +42,7 @@ def Ru():
             print("dir - Посмотреть директорию")
             print("echo - показать текст")
             print("cd - открыть директорию")
+            print("notepad - Блокнот")
         elif cmd == echo:
             global tekst
             tekst = input("Введите текст: ")
@@ -43,7 +56,6 @@ def Ru():
                     papka = 1
                 else:
                     paint("Обнаружена попытка взлома!")
-                    Ru()
             elif Directory == "..":
                 if papka == 1:
                     Directory = "C:/"
@@ -51,14 +63,73 @@ def Ru():
                     print("Ошибка 404 No found")
                     winsound.Beep(1000, 1000)
                     Directory = "C:/"
+            elif cmd == "notepad":
+                print("Блокнот - Безымянный")
+                notepad = input("Впишите свой текст ")
+                global notepadsave
+                notepadsave = input("Сохранить? Y/N ").lower()
+                if notepadsave == "y":
+                    File_two_C = "File.txt"
+                    
+                    
             else: 
                 print("Ошибка 404")
                 winsound.Beep(1000, 1000)
+            
+def En():
+    while True:
+        cmd = input(f"{Directory} > ")
+        if cmd == "":
+            print("Unknown error")
+            winsound.Beep(1000, 1000)
+            time.sleep(5)
+            exit 
+        if cmd == "dir":
+            if papka == 0:
+                print(f"{Folder_one_C}    Folder.   700 Mb ")
+                print(f"{Folder_two_C}.   Folder.   35 Gb")
+                print(f"{File_one_C}    File.   15 Gb ")
+                print(f"{Folder_three_C}.")
+                print(f"{File_two_C}")
+            elif papka == 1:
+                print(f"{name}")
+
+        elif cmd == "help":
+            print("dir - View directory ")
+            print("echo - Display text")
+            print("cd - Select a directory")
+        elif cmd == echo:
+            global tekst
+            tekst = input("Enter text: ")
+            print(f"{tekst}")
+        elif cmd == cd:
+            Directory = input("Select a directory").lower()
+            if Directory == "users":
+                auth = input("Your account password ")
+                if auth == password:
+                    Directory = "C:/users"
+                    papka = 1
+                else:
+                    paint("A hacking attempt was detected.!")
+            elif Directory == "..":
+                if papka == 1:
+                    Directory = "C:/"
+                elif papka == 0:
+                    print("Error 404 No found")
+                    winsound.Beep(1000, 1000)
+                    Directory = "C:/"
+            else: 
+                print("Error 404")
+                winsound.Beep(1000, 1000)
                 
-name = input("Ваше имя ")
-password = input("Пароль ")
+name = input("Ваше имя(Your name) ")
+password = input("Пароль(Password) ")
 auth = ""
-Ru()
+Lang = input("Выбирите язык (Select language): Ru/En ").lower()
+if Lang == "ru":
+    Ru()
+elif Lang == "en"
+    En()
         
         
 
